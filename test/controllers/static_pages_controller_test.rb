321955471117a7e9ef2990b9b3c89c224f493c65
cmthoman <epimyth@gmail.com>
2015-03-28 23:44:50 -0400

    Add tests for Sign up page link and title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup 
+    @basic_title = "| Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
@@ -24,4 +29,5 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
+  
 end
